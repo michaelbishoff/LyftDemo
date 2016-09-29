31bf1e949fe820025ab33ef995c439d3a686e29b
Logan Shire <logan.shire@gmail.com>
2016-09-29 14:45:38 -0700

    moving lyft configuration

--- TestLyft/AppDelegate.swift
+++ TestLyft/AppDelegate.swift
@@ -18,9 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        LyftAPI.ETAs(to: CLLocationCoordinate2D(latitude: 123, longitude: 123)) { result in
-            
-        }
+//        LyftConfiguration.developer = (
         return true
     }
 
