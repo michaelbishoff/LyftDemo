616c3e15a80bcd2a16d3d8556cee462bc996c1c4
Michael Bishoff <michaelbishoff@mbishoff-mbp115.sfhq.lyft-corp.net>
2016-10-01 14:19:40 -0400

    trying a new thing

--- LyftSDK/LyftModels/LyftConfiguration.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// Lyft SDK configuration
-public struct LyftConfiguration {
-    /// Represents the current developer using the LyftSDK. Must be set before any API calls can be made
-    public static var developer: (token: String, clientId: String)?
-}
--- TestLyft/Keys.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-var token = "INSERT_TOKEN_HERE"
-var secret = "INSERT_CLIENT_ID_HERE"
